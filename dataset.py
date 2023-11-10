7a925c4fe44c0412a3b78e7ebc23ca96029be127
GCPhillips <grant.christopher.phillips@gmail.com>
2023-11-09 18:43:28 -0600

    refactored; added mel spectrogram as transformation

--- dataset.py
+++ dataset.py
@@ -1,43 +1,59 @@
-import numpy as np
 import torch.cuda
+import torchaudio.functional
 
-from utils import stereo_to_mono, INST_DICT
+from utils import INST_DICT
 from torch.utils.data import Dataset, DataLoader
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-SAMPLE_RATE = 44100
-SAMPLES = (SAMPLE_RATE * 3) - 1  # 3 seconds at 44.1 khz, one less sample
+SAMPLE_RATE = 15000
+SECONDS = 1
+SAMPLES = SAMPLE_RATE * SECONDS
+N_FFT = 400
 
 class InstrumentDataset(Dataset):
-    def __init__(self, irmas_tracks, track_ids):
-        self.tracks = []
-        self.labels = []
+    def __init__(self, irmas_tracks, track_ids, source_sample_rate, num_classes, device):
+        self.tracks = track_ids
+        self.irmas_tracks = irmas_tracks
+        self.mel_spectrogram = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, N_FFT).to(device)
+        self.resample = torchaudio.transforms.Resample(source_sample_rate, SAMPLE_RATE).to(device)
+        self.num_classes = num_classes
+        self.device = device
 
-        for id in track_ids:
-            track = irmas_tracks[id]
-            audio = track.audio[0]
-            if len(audio) == 2:
-                audio = stereo_to_mono(audio)
-            if audio.shape[0] > SAMPLES:
-                audio = audio[0:SAMPLES]
-            label = np.zeros(len(INST_DICT))
-            for instr in track.instrument:
-                label[INST_DICT.index(instr)] = 1.0
-            label = torch.from_numpy(label)
-            label = label.to(device)
-            audio = torch.from_numpy(audio)
-            audio = audio.to(device)
-            self.tracks.append(audio)
-            self.labels.append(label)
+    def _transform_audio(self, track):
+        audio = torch.from_numpy(track.audio[0]).to(self.device)
+        sr = track.audio[1]
+        if audio.shape[0] > 1:
+            audio = torch.mean(audio, dim=0, keepdim=True)
+        if sr != SAMPLE_RATE:
+            audio = self.resample(audio)
+        audio_len = audio.shape[1]
+        if audio_len > SAMPLES:
+            audio = audio[:, :SAMPLES]
+        if audio_len < SAMPLES:
+            offset = SAMPLES - audio_len
+            padding = (0, offset)
+            audio = torch.nn.functional.pad(audio, padding)
+        audio = self.mel_spectrogram(audio)
+        return audio
+
+    def _create_label(self, instrument_list):
+        label = torch.zeros(self.num_classes)
+        for instrument in instrument_list:
+            label[INST_DICT.index(instrument)] = 1
+        return label
 
     def __len__(self):
         return len(self.tracks)
 
     def __getitem__(self, item):
-        return self.tracks[item], self.labels[item]
+        track = self.irmas_tracks[self.tracks[item]]
+        audio = self._transform_audio(track)
+        label = self._create_label(track.instrument)
+        return audio, label.to(self.device)
 
 
 def load_data(irmas_tracks, track_ids, num_workers=0, batch_size=128, shuffle=True, drop_last=True):
-    dataset = InstrumentDataset(irmas_tracks, track_ids)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    dataset = InstrumentDataset(irmas_tracks, track_ids, 44100, len(INST_DICT), device)
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
